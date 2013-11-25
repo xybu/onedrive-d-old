4a628a9c536256de9306c58c357a75d564796256
Xiangyu Bu <xybu92@live.com>
2013-11-24 20:43:47 -0500

    More elegant inst.sh

--- src/inst.sh
+++ src/inst.sh
@@ -1,29 +1,22 @@
-echo "Checking if package \"python-pip\" is installed..."
-dpkg-query -l python-pip
-if [ $? -eq 1 ]; then
-	echo "Installing package python-pip..."
-	sudo apt-get -y install python-pip
-fi
+install_pkg() {
+	echo "Checking if package \"$1\" is installed..."
+	dpkg-query -l $1 > tmp.txt
+	if [ $? -eq 1 ]; then
+		echo "Installing package $1..."
+		sudo apt-get -y install $1
+	else
+		echo "Package $1 is installed. Skip this step."
+	fi
+	rm tmp.txt
+}
 
-echo "Checking if package \"libyaml\" is installed..."
-dpkg-query -l libyaml-dev
-if [ $? -eq 1 ]; then
-	echo "Installing package libyaml-dev..."
-	sudo apt-get -y install libyaml-dev
-fi
-
-echo "Installing PyYAML..."
-#pip search yaml
-#pip install pyyaml
-sudo apt-get install python-yaml
-
-#echo "Cleaning..."
-#rm -rfv PyYAML-3.10
+install_pkg python-pip
+install_pkg libyaml-dev
+install_pkg python-yaml
 
 # Note that to install stuff in system-wide PATH and site-packages, 
 # elevated privileges are often required.
 # Use "install --user", ~/.pydistutils.cfg or
 # virtualenv to do unprivileged installs into custom paths.
-
-echo "Upgrading python-skydrive..."
+echo "Installing/Upgrading python-skydrive..."
 sudo pip install 'git+https://github.com/mk-fg/python-skydrive.git#egg=python-skydrive[standalone]' --upgrade
