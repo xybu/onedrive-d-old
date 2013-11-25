d30b344df0923583a9e41db2bf1b3a5900dd08e5
Xiangyu Bu <xybu92@live.com>
2013-11-24 20:35:51 -0500

    Simplified inst.sh

--- src/inst.sh
+++ src/inst.sh
@@ -12,18 +12,18 @@ if [ $? -eq 1 ]; then
 	sudo apt-get -y install libyaml-dev
 fi
 
+echo "Installing PyYAML..."
+#pip search yaml
+#pip install pyyaml
+sudo apt-get install python-yaml
+
+#echo "Cleaning..."
+#rm -rfv PyYAML-3.10
+
 # Note that to install stuff in system-wide PATH and site-packages, 
 # elevated privileges are often required.
 # Use "install --user", ~/.pydistutils.cfg or
 # virtualenv to do unprivileged installs into custom paths.
-echo "Installing python-skydrive..."
-sudo pip install 'git+https://github.com/mk-fg/python-skydrive.git#egg=python-skydrive[standalone]'
 
-echo "Installing PyYAML..."
-#cd ~ && 
-wget -O - "http://pyyaml.org/download/pyyaml/PyYAML-3.10.tar.gz" | tar xzf -
-cd PyYAML-3.10 && sudo python2 setup.py --with-libyaml install
-cd ..
-
-echo "Cleaning..."
-rm -rfv PyYAML-3.10
+echo "Upgrading python-skydrive..."
+sudo pip install 'git+https://github.com/mk-fg/python-skydrive.git#egg=python-skydrive[standalone]' --upgrade
