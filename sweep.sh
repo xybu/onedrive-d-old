3b913d151a419217cd0162a2ee260bd57d8d651d
Xiangyu Bu <xybu92@live.com>
2014-04-02 22:37:46 -0700

    Added scanner number control, exclusion list, etc.

--- sweep.sh
+++ sweep.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-ps -e | grep onedrive-d | cut -d' ' -f2 | xargs kill -9
-# killall onedrive-d -v
-ps -e | grep inotifywait | cut -d' ' -f2 | xargs kill -9
-# killall inotifywait -v
+#ps -e | grep onedrive-d | cut -d' ' -f2 | xargs kill -9
+killall -9 onedrive-daemon
+#ps -e | grep inotifywait | cut -d' ' -f2 | xargs kill -9
+killall -9 inotifywait
+killall -9 python
