48847f07c906566a2b90de13c9ffae59a0dbe051
Xiangyu Bu <xybu92@live.com>
2014-04-03 18:35:25 -0700

    Use LiveAPI to list files

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
 	install_requires = ['PyYAML', 'requests', 'urllib3', 'python-skydrive'],
 	
 	packages=find_packages(),
-	include_package_data=True,
+	# include_package_data=True,
 	scripts=['onedrive/onedrive-daemon','onedrive/onedrive-utils'],
 	exclude_package_data={'': ['README.*']}
 
